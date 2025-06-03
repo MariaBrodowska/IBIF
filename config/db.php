2bd662ddc67480db42871ef76af6d98a549de91b
Maria <mariabrodowska89@gmail.com>
2025-06-03 22:44:08 +0200

    Initial project setup - structure, config

---
+++ config/db.php
@@ -0,0 +1,24 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$db   = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
+$charset = $_ENV['DB_CHARSET'];
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     die('DB connection failed: ' . $e->getMessage());
+}
---
+++ public/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+  </head>
+  <body>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </body>
+</html>
+
+<!-- <?php
+
+require_once __DIR__ . '/../config/db.php';
+require_once __DIR__ . '/../app/Core/App.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use App\Core\App;
+
+session_start();
+
+$app = new App();
+$app->run();
+?> -->
